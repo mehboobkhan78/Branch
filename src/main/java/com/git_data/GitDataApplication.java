2a487936844f71968dea94ca14581007205d0817
mehboob <mehboobkarmadi@gmail.com>
2025-04-29 15:17:18 +0530

    adding the file

---
+++ src/main/java/com/git_data/GitDataApplication.java
@@ -0,0 +1,15 @@
+package com.git_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDataApplication {
+
+	public static void main(String[] args) {
+		int devmem= 100;
+		boolean devmem = true;
+		SpringApplication.run(GitDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git_data/GitDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
